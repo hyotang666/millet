667bf1976e70649f892cc9158381eb11a2535f01
SATO Shinichi <hyotang.karakoma@gmail.com>
2021-10-17 01:37:20 +0900

    Fix: default.

--- src/default.lisp
+++ src/default.lisp
@@ -2,31 +2,33 @@
 
 (eval-when (:compile-toplevel :load-toplevel)
   (defun already-defined-p (symbol)
-    (if (fboundp symbol)
+    (if (fboundp
+          (or (find-symbol (string symbol) (find-package :millet))
+              (error "Missing ~S." symbol)))
         '(and)
         '(or))))
 
-#+#.(millet::already-defined-p 'function-name)
+#-#.(millet::already-defined-p :function-name)
 (defun function-name (function)
   (check-type function function)
   (nth-value 2 (function-lambda-expression function)))
 
-#+#.(millet::already-defined-p 'global-symbol-p)
+#-#.(millet::already-defined-p :global-symbol-p)
 (defun global-symbol-p (symbol)
   (declare (ignore symbol))
   (not-support 'global-symbol-p))
 
-#+#.(millet::already-defined-p 'special-symbol-p)
+#-#.(millet::already-defined-p :special-symbol-p)
 (defun special-symbol-p (symbol)
   (and (global-symbol-p symbol) (not (constantp symbol))))
 
-#+#.(millet::already-defined-p 'lambda-list)
+#-#.(millet::already-defined-p :lambda-list)
 (defun lambda-list (arg) (declare (ignore arg)) (not-support 'lambda-list))
 
-#+#.(millet::already-defined-p 'type-expand)
+#-#.(millet::already-defined-p :type-expand)
 (defun type-expand (type) (declare (ignore type)) (not-support 'type-expand))
 
-#+#.(millet::already-defined-p 'type-specifier-p)
+#-#.(millet::already-defined-p :type-specifier-p)
 (defun type-specifier-p (type)
   (labels ((rec (specifier)
              (cond
