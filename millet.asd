90044281a13678288da476d9ddc231850b892d45
SATO Shinichi <hyotang.karakoma@gmail.com>
2021-10-30 23:31:03 +0900

    Fix: Default functions.
    Old:	Just checking already FBOUND or not.
    Issue:	When millet is recompiled, fasl lose default functions.
    New:	Fixed with marking default functions.

--- millet.asd
+++ millet.asd
@@ -1,7 +1,7 @@
 ; vim: ft=lisp et
 (in-package :asdf)
 (defsystem :millet
-  :version "0.4.10"
+  :version "0.4.11"
   :description "Wrapper for implementation dependent tiny utilities."
   :long-description #.(uiop:read-file-string
                         (uiop:subpathname *load-pathname* "README.md"))
--- src/default.lisp
+++ src/default.lisp
@@ -1,44 +1,64 @@
 (in-package :millet)
 
 (eval-when (:compile-toplevel :load-toplevel)
-  (defun already-defined-p (symbol)
-    (if (fboundp
-          (or (find-symbol (string symbol) (find-package :millet))
-              (error "Missing ~S." symbol)))
-        '(:and)
-        '(:or))))
+  ;; In order to distinguish the default functions and implementation-specific functions,
+  ;; we set symbol-plist as the mark of default functions.
+  ;; Otherwise, when millet is recompiled,
+  ;; fasl will lose default functions.
+  (defun mark-as-default (symbol)
+    "Set the mark of the millet default function to symbol-plist."
+    (setf (get symbol 'millet-default) t)
+    symbol)
+  (defun defaultp (symbol)
+    "Is the default function?"
+    (get symbol 'millet-default))
+  (defun already-defined-p (name)
+    (let ((symbol (find-symbol (string name) :millet)))
+      (unless symbol
+        (error "Missing ~S." symbol))
+      (and (fboundp symbol) (not (defaultp symbol))))))
 
-#-#.(millet::already-defined-p :function-name)
-(defun function-name (function)
-  (check-type function function)
-  (nth-value 2 (function-lambda-expression function)))
+#.(if (already-defined-p :function-name)
+      (values)
+      `(defun ,(mark-as-default 'function-name) (function)
+         (check-type function function)
+         (nth-value 2 (function-lambda-expression function))))
 
-#-#.(millet::already-defined-p :global-symbol-p)
-(defun global-symbol-p (symbol)
-  (declare (ignore symbol))
-  (not-support 'global-symbol-p))
+#.(if (already-defined-p :global-symbol-p)
+      (values)
+      `(defun ,(mark-as-default 'global-symbol-p) (symbol)
+         (declare (ignore symbol))
+         (not-support 'global-symbol-p)))
 
-#-#.(millet::already-defined-p :special-symbol-p)
-(defun special-symbol-p (symbol)
-  (and (global-symbol-p symbol) (not (constantp symbol))))
+#.(if (already-defined-p :special-symbol-p)
+      (values)
+      `(defun ,(mark-as-default 'special-symbol-p) (symbol)
+         (and (global-symbol-p symbol) (not (constantp symbol)))))
 
-#-#.(millet::already-defined-p :lambda-list)
-(defun lambda-list (arg) (declare (ignore arg)) (not-support 'lambda-list))
+#.(if (already-defined-p :lambda-list)
+      (values)
+      `(defun ,(mark-as-default 'lambda-list) (arg)
+         (declare (ignore arg))
+         (not-support 'lambda-list)))
 
-#-#.(millet::already-defined-p :type-expand)
-(defun type-expand (type) (declare (ignore type)) (not-support 'type-expand))
+#.(if (already-defined-p :type-expand)
+      (values)
+      `(defun ,(mark-as-default 'type-expand) (type)
+         (declare (ignore type))
+         (not-support 'type-expand)))
 
-#-#.(millet::already-defined-p :type-specifier-p)
-(defun type-specifier-p (type)
-  (labels ((rec (specifier)
-             (cond
-               ((typep specifier '(cons (member and or not)))
-                (every #'rec (cdr specifier)))
-               ;; At least ECL needs.
-               ((or (and (symbolp specifier)
-			 (not (keywordp specifier)))
-		    (consp specifier)
-                    (typep specifier 'standard-class))
-                (values (ignore-errors (progn (typep '#:dummy specifier) t))))
-               (t nil))))
-    (rec type)))
+#.(if (already-defined-p :type-specifier-p)
+      (values)
+      `(defun ,(mark-as-default 'type-specifier-p) (type)
+         (labels ((rec (specifier)
+                    (cond
+                      ((typep specifier '(cons (member and or not)))
+                       (every #'rec (cdr specifier)))
+                      ;; At least ECL needs.
+                      ((or (and (symbolp specifier) (not (keywordp specifier)))
+                           (consp specifier)
+                           (typep specifier 'standard-class))
+                       (values (ignore-errors
+                                (progn (typep '#:dummy specifier) t))))
+                      (t nil))))
+           (rec type))))
